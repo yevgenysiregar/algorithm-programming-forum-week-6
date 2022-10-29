94825240b2fb166750d58070265918ec84a8bbdd
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2022-10-29 20:21:03 +0700

    MCQ Questions

---
+++ MCQ.py
@@ -0,0 +1,11 @@
+#Answers for MCQ
+
+# 1. (b) 2
+# 2. (D) This code produces an error
+# 3. (a) c
+# 4. (b) 0 1 2
+# 5. (b) 0 1 2
+# 6. (c) (’a’, 0) (’b’, 1) (’c’, 2)
+# 7. (a) a b c
+# 8. (c) dc
+# 9. (b) george john thomas
